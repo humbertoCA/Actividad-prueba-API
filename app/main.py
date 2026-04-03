da699152a334ce06721237ba1c9618af246ff090
Humberto Coronel <h.coronelalar@gmail.com>
2026-04-03 15:28:52 -0600

    fix gitignore and remove unnecessary files

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routes.upload import router as upload_router #Agregamos el router al main
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "API funcionando 🚀"}
-app.include_router(upload_router)
